3843bde4a9f17d7123efc676a1e8abab654abc97
Igor Kindop <Igor.Kindop@gmail.com>
2025-05-28 21:56:32 +0200

    creating a SQLite DB and ensuring simple CRUD

--- fastapi_start/sql_example/main.py
+++ fastapi_start/sql_example/main.py
@@ -1,6 +1,8 @@
-from database import SessionLocal
-from fastapi import Depends, FastAPI
+from database import SessionLocal, User
+from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
+from pydantic import BaseModel
+
 
 def get_db():
     db=SessionLocal()
@@ -11,9 +13,63 @@ def get_db():
         
 app = FastAPI()
 
-@app.get("/users/")
+#CRUD operations
+
+class UserBody(BaseModel):
+    name: str
+    email: str
+
+
+@app.post("/user") # to add a new user to the database
+def add_user(user: UserBody, 
+             db: Session = Depends(get_db)):
+    new_user = User(
+        name = user.name,
+        email = user.email
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.get("/users") # to read all users from the database
 def read_users(db: Session = Depends(get_db)):
-    users = db.query(user).all()
+    users = db.query(User).all()
     return users
 
-#CRUD operations
+@app.get("/user") # to read a user by id from the database
+def get_user(user_id: int, 
+             db: Session = Depends(get_db)):
+    user = (db.query(User).filter(User.id==user_id).first())
+    if user is None:
+        raise HTTPException(status_code=404, detail = "User not found")
+    return user
+
+@app.post("/user/{user_id}") # to update a user by id in the database
+def update_user(
+        user_id: int, 
+        user: UserBody,
+        db: Session = Depends(get_db)):
+    db_user = (
+        db.query(User).filter(User.id == user_id).first() # to get the first user with matching ID
+    )
+    if db_user is None:
+        raise HTTPException(status_code=404, detail = "User not found")
+    db_user.name = user.name
+    db_user.email = user.email
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+@app.delete("/user") # to delete a user from the database using ID
+def delete_user(
+    user_id: int,
+    db: Session = Depends(get_db)
+):
+    db_user = (db.query(User).filter(User.id==user_id).first())
+    if db_user is None:
+        raise HTTPException(status_code=404, detail = "User not found")
+    db.delete(db_user)
+    db.commit()
+    return {"detail": "user deleted"}
